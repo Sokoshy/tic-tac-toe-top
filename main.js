3b282e6599e361006f087b0a456e1e0e1061e2d1
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-09-21 11:40:59 +0200

    feat(js): add createPlayer factory

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ const gameBoard = (() => {
   return {getGameBoard};
 })();
 
+function createPlayer(name, mark) {
+  return {name: name, mark: mark};
+}
+
+const p1 = createPlayer("jean", "X");
+const p2 = createPlayer("marc", "O");
+
+console.log(p1.mark, p2.mark);
+console.log(p1.name, p2.name);
