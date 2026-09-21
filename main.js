f891bce7e029f0e0461986f5255c33c44a292fc5
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-09-21 12:39:34 +0200

    feat(js): add gameController with turn management

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("setup ok")
 const gameBoard = (() => {
   let board = new Array(9).fill("");
 
-  const placeMark = (mark, index) => {
+  const placeMark = (index, mark) => {
     if(board[index] === ""){
       board[index] = mark;
       return true;
@@ -21,6 +21,27 @@ function createPlayer(name, mark) {
 
 const p1 = createPlayer("jean", "X");
 const p2 = createPlayer("marc", "O");
+
+const gameController = (() => {
+  let lastTurn = "";
+
+  const lastPlayed = () => lastTurn; 
+
+  const actualTurn= (index, player) => {
+    if(lastTurn !== player.name) {
+      if(gameBoard.placeMark(index, player.mark) === false){
+        return "The case is not empty";
+      }else{
+        lastTurn = player.name;
+        return "The placement is correct";
+      }
+    }else {
+      return "the player has already played";
+    }
+  }
+
+  return {lastPlayed, actualTurn}
+})();
 
 console.log(p1.mark, p2.mark);
 console.log(p1.name, p2.name);
